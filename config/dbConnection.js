3e688ec32f303048bd29b3f4b4e2744c1b93fbf8
Vinay Datir <vdatir910@gmail.com>
2023-04-24 01:39:02 +0530

    Made connection properly

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () =>{
-    try{
+const connectDB = async () => {
+    try {
         const db = process.env.MONGO_URI;
-        await mongoose.connect(db);
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("Database connected!!");
-    }catch(err){
+    } catch (err) {
         console.log(err);
         process.exit(1);
     }
 }
+
 module.exports = connectDB;
